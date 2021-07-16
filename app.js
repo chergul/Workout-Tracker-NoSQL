b212f59c0e3ef286d6f62e269b303c538396f68b
Ceyda Hergul <ceyda_hergul@hotmail.com>
2021-07-16 14:07:04 -0400

    updates

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const PORT = process.env.PORT || 3000
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// const databaseUrl = "notetaker";
+// const collections = ["notes"];
+
+// const db = mongojs(databaseUrl, collections);
+
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
+
 
 // Set the app to listen on port 3000
 app.listen(3000, () => {
