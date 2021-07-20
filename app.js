28b8da3f043283dcc3af3f28b6c5a19569d6cf18
Ceyda Hergul <ceyda_hergul@hotmail.com>
2021-07-20 13:32:09 -0400

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongojs = require("mongojs");
 const mongoose = require("mongoose");
+
 // HTTP request logger middleware
 const logger = require("morgan");
 
@@ -14,6 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+//Bind database
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
